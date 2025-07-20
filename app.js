e5c5ace6aeec657019f1ef42d01881b2605dbbb3
IrvDelgado <irvingdelgado6@gmail.com>
2025-07-20 01:55:59 -0600

    rate-limiter

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const routes = require('./src/routes');
 const errorHandler = require('./src/middleware/errorHandler');
 
 const app = express();
+app.set('trust proxy', true);
 app.use(cors());
 app.use(express.json());
 app.use(limiter);
